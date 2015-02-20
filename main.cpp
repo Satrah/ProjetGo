8a60f6f67cb80a803fb2e0e8a3e5c963bd8a45c5
Daemon en local <Daemon@local>
2015-02-20 13:53:00 +0100

    cmake files structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "GOProject/ImageLoader.h"
 
 int main()
 {
+    GOProject::ImageLoader loader;
+    loader.Load("img/test1.jpg");
+    loader.DebugDisplay();
     return 0;
 }
