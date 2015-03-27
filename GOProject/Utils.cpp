26ab2e7873121ad217431c9b3b8c6029e4c98d85
GOProjectDev1 <goproject@modalimage>
2015-03-27 14:48:36 +0100

    [Utils] Add line intersection / rectangle fill with color

--- GOProject/Utils.cpp
+++ GOProject/Utils.cpp
@@ -70,3 +70,37 @@ void equalizeHist(cv::InputArray _src, cv::OutputArray _dst, cv::InputArray _mas
 	CvMat _csrc = src, _cdst = dst, _cmask = mask;
 	cvEqualizeHist(&_csrc, &_cdst, &_cmask);
 }
+
+// Finds the intersection of two lines, or returns false.
+// The lines are defined by (o1, p1) and (o2, p2).
+bool intersection(cv::Point2f o1, cv::Point2f p1, cv::Point2f o2, cv::Point2f p2,
+	cv::Point2f &r)
+{
+	cv::Point2f x = o2 - o1;
+	cv::Point2f d1 = p1 - o1;
+	cv::Point2f d2 = p2 - o2;
+
+	float cross = d1.x*d2.y - d1.y*d2.x;
+	if (abs(cross) < /*EPS*/1e-8)
+		return false;
+
+	double t1 = (x.x * d2.y - x.y * d2.x) / cross;
+	r = o1 + d1 * t1;
+	return true;
+}
+
+
+void fillRectangle(cv::OutputArray mat, cv::RotatedRect rect)
+{
+	cv::Scalar color(1);
+	rect.size.height += 100;
+	rect.size.width += 100;
+	cv::Point2f rectPoints[4];
+	cv::Point polyPoints[4];
+	int nPoints[] = { 4 };
+	rect.points(rectPoints);
+	for (int i = 0; i < 4; ++i)
+		polyPoints[i] = cv::Point(rectPoints[i].x, rectPoints[i].y);
+	const cv::Point* ppt[1] = { polyPoints };
+	fillPoly(mat.getMat(), (const cv::Point**)ppt, nPoints, 1, color);
+}
