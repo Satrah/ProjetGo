d1588016afbb43c1495982d460108e635b364dc8
Sarah <sarah.fachada@outlook.fr>
2015-04-02 18:00:02 +0200

    debut liberte

--- GOProject/AlGo.h
+++ GOProject/AlGo.h
@@ -20,11 +20,14 @@ namespace GOProject
 		void refresh(ImageLoader const& loader);
 		void suggereCoup(ImageLoader const& loader);
 		void affichePlateau();
+		void calculLibertes();
 		AlGo(){ Image<uchar> I;  for (int i = 0; i < MEMORY_FRAMES; ++i) _memory.push_back(I); }
 	protected:
 		static const int MEMORY_FRAMES = 40;
 		typedef std::pair<int, int> BoardPosition;
+		int _calculLibAux(int i, int j, std::map<BoardPosition, bool> calcule);
 		std::map<BoardPosition, EtatCase> _plateau;
+		std::map<BoardPosition, int> _libertes;
 		int _taillePlateau;
 		std::vector<Image<uchar>> _memory;
 		int _current = 0;
